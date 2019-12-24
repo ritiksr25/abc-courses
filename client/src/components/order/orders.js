c341018e3eed925de26fc340ae12b077589f960b
ritik srivastava <ritiksrivastava025@gmail.com>
2019-12-24 14:59:09 +0530

    pending payment completion

--- client/src/components/order/orders.js
+++ client/src/components/order/orders.js
@@ -20,45 +20,59 @@ class Orders extends Component {
     render() {
         const data = this.state.data.data;
         return (
-                <div className="container pt-4 pb-4">
-			<div className="row">
-				<div className="col-sm-8 offset-sm-2 text-center">
-					<h2>Order History</h2>
-				</div>
-            </div>
             <div className="container pt-4 pb-4">
-            <div className="row">
-			<div className="col-md-8 mx-auto" >
-                <div className="card">
-                    <table className="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">S.No.</th>
-                            <th scope="col">Course</th>
-                            <th scope="col">Payment Status</th>
-                            <th scope="col">Purchased On</th>
-                            <th scope="col">?</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        {data
-                            ? data.orders.map((item, i) => (
-                          <tr key={i}>
-                            <th scope="row">{i+1}</th>
-                            <td>{item.course.title}</td>
-                            <td>{item.status}</td>
-                            <td>{item.createdAt.slice(0,10)}</td>
-                            <td><Link to={`/orders/${item._id}`}>View</Link></td>
-                          </tr>
-                         ))
-                         : null}
-    
-                        </tbody>
-                      </table>
+                <div className="row">
+                    <div className="col-sm-8 offset-sm-2 text-center">
+                        <h2>Order History</h2>
+                    </div>
                 </div>
-            </div>
-            </div>
-            </div>
+                <div className="container pt-4 pb-4">
+                    <div className="row">
+                        <div className="col-md-8 mx-auto">
+                            <div className="card">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">S.No.</th>
+                                            <th scope="col">Course</th>
+                                            <th scope="col">Payment Status</th>
+                                            <th scope="col">Purchased On</th>
+                                            <th scope="col">?</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data
+                                            ? data.orders.map((item, i) => (
+                                                  <tr key={i}>
+                                                      <th scope="row">
+                                                          {i + 1}
+                                                      </th>
+                                                      <td>
+                                                          {item.course.title}
+                                                      </td>
+                                                      <td>{item.status}</td>
+                                                      <td>
+                                                          {item.createdAt.slice(
+                                                              0,
+                                                              10
+                                                          )}
+                                                      </td>
+                                                      <td>
+                                                          <Link
+                                                              to={`/orders/${item._id}`}
+                                                          >
+                                                              View
+                                                          </Link>
+                                                      </td>
+                                                  </tr>
+                                              ))
+                                            : null}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- client/src/components/order/single.js
+++ client/src/components/order/single.js
@@ -3,27 +3,106 @@ import banner from "./banner.jpg";
 import axios from "axios";
 import * as ROUTES from "../../utils/routes";
 
-
 class Order extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          data: "",
-          token: ""
+            data: "",
+            token: ""
         };
-      }
+    }
 
     async componentDidMount() {
         let token = localStorage.getItem("token");
-        const { data } = await axios.get(`${ROUTES.order}?id=${this.props.match.params.id}`, {
-            headers: {
-                "x-auth-token": token
+        const { data } = await axios.get(
+            `${ROUTES.order}?id=${this.props.match.params.id}`,
+            {
+                headers: {
+                    "x-auth-token": token
+                }
             }
-        });
+        );
         this.setState({ data: data, token: token });
     }
-render(){
-const data = this.state.data.data;
+
+    handlePayment = async (e, item) => {
+        e.preventDefault();
+
+        let token = this.state.token;
+        try {
+            // const { data } = await axios.post(
+            // 	`${ROUTES.order}/${item._id}`,
+            // 	{},
+            // 	{
+            // 		headers: {
+            // 			"x-auth-token": token
+            // 		}
+            // 	}
+            // );
+            const res = await axios.post(
+                `${ROUTES.payorder}/${item._id}`,
+                {},
+                {
+                    headers: {
+                        "x-auth-token": token
+                    }
+                }
+            );
+            const options = {
+                key: res.data.key,
+                name: "Payments",
+                amount: res.data.order.amount,
+                order_id: res.data.order.id,
+                notes: res.data.order.notes,
+
+                handler: async response => {
+                    try {
+                        const resp = await axios.post(
+                            ROUTES.paysuccess,
+                            {
+                                razorpay_payment_id:
+                                    response.razorpay_payment_id,
+                                razorpay_order_id: response.razorpay_order_id,
+                                razorpay_signature: response.razorpay_signature
+                            },
+                            {
+                                headers: {
+                                    "x-auth-token": token
+                                }
+                            }
+                        );
+
+                        if (resp.data.message === "success") {
+                            alert(
+                                "Payment successfully completed. Redirecting to order page"
+                            );
+                            return (
+                                <Redirect to={"/orders" + resp.data.data._id} />
+                            );
+                        }
+                    } catch (err) {
+                        if (err.response === undefined) {
+                            console.log(err.message);
+                        } else {
+                            console.log(err.response);
+                        }
+                    }
+                },
+                theme: {
+                    color: "#9D50BB"
+                }
+            };
+            const rzp1 = new window.Razorpay(options);
+            rzp1.open();
+        } catch (error) {
+            if (error.response !== undefined) {
+            } else {
+            }
+        }
+    };
+
+    render() {
+        const data = this.state.data.data;
         return (
             <div>
                 <div
@@ -39,7 +118,9 @@ const data = this.state.data.data;
                                     <img
                                         className="card-img-top"
                                         src={
-                                            data.course.image ? data.course.image.url : banner
+                                            data.course.image
+                                                ? data.course.image.url
+                                                : banner
                                         }
                                         height="200px"
                                         width="200px"
@@ -64,24 +145,46 @@ const data = this.state.data.data;
                                     </span>
                                 </div>
                                 <div className="card">
-                                <p className="card-block">
-                        <strong className=" description">Purchased On: </strong>{data.createdAt.slice(0,10)}
-                      </p>
-                      <p class="card-block">
-                        <strong className=" description">Payment Status: </strong>{data.status}
-                      </p>
-                      <p class="card-block">
-                        <strong className=" description">Transaction ID: </strong>{data.transacId}
-                      </p>
-                      <p class="card-block">
-                        <strong className=" description">Invoice ID: </strong>{data.invoiceId}
-                      </p>
+                                    <p className="card-block">
+                                        <strong className=" description">
+                                            Purchased On:{" "}
+                                        </strong>
+                                        {data.createdAt.slice(0, 10)}
+                                    </p>
+                                    <p class="card-block">
+                                        <strong className=" description">
+                                            Payment Status:{" "}
+                                        </strong>
+                                        {data.status}
+                                    </p>
+                                    <p class="card-block">
+                                        <strong className=" description">
+                                            Transaction ID:{" "}
+                                        </strong>
+                                        {data.transacId}
+                                    </p>
+                                    <p class="card-block">
+                                        <strong className=" description">
+                                            Invoice ID:{" "}
+                                        </strong>
+                                        {data.invoiceId}
+                                    </p>
                                 </div>
                             </div>
+                        ) : null}
+                        {data.status === "pending" ? (
+                            <button
+                                className="btn btn-outline-dark sec_btn custom_btn ml-0 mb-2"
+                                onClick={e => this.handlePayment(e, data)}
+                            >
+                                Make Payment
+                            </button>
                         ) : null}
                     </div>
                 </div>
             </div>
-        )}}
+        );
+    }
+}
 
-        export default Order;
+export default Order;
