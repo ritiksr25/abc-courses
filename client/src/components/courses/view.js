86edd8e1445560c7ef9c14a85f9a4afde003ca4d
ritik srivastava <ritiksrivastava025@gmail.com>
2019-12-29 22:43:23 +0530

    test gtranslate widget

--- client/src/components/courses/view.js
+++ client/src/components/courses/view.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Redirect } from 'react-router'
 import banner from "./banner.jpg";
 import axios from "axios";
